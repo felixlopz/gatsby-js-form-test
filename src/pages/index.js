a537dad9b8a222786412e83eb01bade756048c16
felixlopzd <felixlopzd@gmail.com>
2021-05-23 14:27:05 -0400

    class base componetn

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,72 +2,76 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import * as qs from 'query-string';
 
-const IndexPage = () => {
-  const [email, setEmail] = useState('');
-  const [name, setName] = useState('');
-  const [message, setMessage] = useState('');
-
-  function handleSubmit(event) {
-    event.preventDefault();
-
-    const formData = { name: name, email: email, message: message };
-
-    const axiosOptions = {
-      url: '/',
-      method: 'post',
-      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
-      data: qs.stringify(formData),
-    };
-
-    axios(axiosOptions)
-      .then((response) => {
-        console.log('Form submitted');
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }
-
-  return (
-    <form
-      name="contact"
-      onSubmit={handleSubmit}
-      method="POST"
-      data-netlify="true"
-    >
-      <div>
-        <label htmlFor="name-input">Introduzca su nombre</label>
-        <input
-          type="text"
-          name="name"
-          id="name-input"
-          value={name}
-          onChange={(e) => setName(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email-input">Introduzca su email</label>
-        <input
-          type="email"
-          name="email"
-          id="email-input"
-          value={email}
-          onChange={(e) => setEmail(e.target.value)}
-        />
-      </div>
-      <div>
-        <label htmlFor="email-input">Introduzca su nombre</label>
-        <textarea
-          type="text"
-          name="message"
-          id="message-input"
-          value={message}
-          onChange={(e) => setMessage(e.target.value)}
-        />
-      </div>
-      <button type="submit">submit</button>
-    </form>
-  );
+const encode = (data) => {
+  return Object.keys(data)
+    .map((key) => encodeURIComponent(key) + '=' + encodeURIComponent(data[key]))
+    .join('&');
 };
 
-export default IndexPage;
+class ContactForm extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { name: '', email: '', message: '' };
+  }
+
+  /* Here’s the juicy bit for posting the form submission */
+
+  handleSubmit = (e) => {
+    fetch('/', {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
+      body: encode({ 'form-name': 'contact', ...this.state }),
+    })
+      .then(() => alert('Success!'))
+      .catch((error) => alert(error));
+
+    e.preventDefault();
+  };
+
+  handleChange = (e) => this.setState({ [e.target.name]: e.target.value });
+
+  render() {
+    const { name, email, message } = this.state;
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <p>
+          <label>
+            Your Name:{' '}
+            <input
+              type="text"
+              name="name"
+              value={name}
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Your Email:{' '}
+            <input
+              type="email"
+              name="email"
+              value={email}
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <label>
+            Message:{' '}
+            <textarea
+              name="message"
+              value={message}
+              onChange={this.handleChange}
+            />
+          </label>
+        </p>
+        <p>
+          <button type="submit">Send</button>
+        </p>
+      </form>
+    );
+  }
+}
+
+export default ContactForm;
